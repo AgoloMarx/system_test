04ee749a7601c2ded60558eaa7c69cddfbb25264
marxlow <marxlow94@gmail.com>
2018-05-23 16:31:07 -0400

    Add in puppeteer teardown for hook

--- features/hooks.js
+++ features/hooks.js
@@ -1,7 +1,10 @@
+
 const { BeforeAll, AfterAll } = require('cucumber');
 const puppeteer = require('puppeteer');
 
+// BeforeAll and AfterAll are run after ALL scenarios. Not each.
 BeforeAll(async () => {
+  // Set up Puppeteer
   const browser = await puppeteer.launch({
     headless: false,
     slowMo: 500,
@@ -10,5 +13,7 @@ BeforeAll(async () => {
 });
 
 AfterAll(async () => {
+  // Teardown browser
   await this.browser.close();
 })
+
