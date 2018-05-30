19acd46096d46aa834deefab1cc4e852fb94740b
AgoloMarx <marx.low@agolo.com>
2018-05-30 14:26:22 -0400

    Change feed name

--- features/steps/createFeed.js
+++ features/steps/createFeed.js
@@ -1,13 +1,13 @@
 const assert = require('assert');
 const { Given, When, Then } = require('cucumber');
 
-const TEST_FEED_NAME = 'a cucumber test';
+const TEST_FEED_NAME = ' - a cucumber test';
 const TEST_KEYWORDS = 'Nicholas Cage'; // Gives us exactly 1 article
 
 When('he creates {int} feeds in parallel', {timeout: 10 * 1000}, async function (numFeed) {
   this.createdFeeds = [ ];
   for (let i = 0; i < 1; i++) {
-    await this.createFeed(`${TEST_FEED_NAME}${i}`, TEST_KEYWORDS);
+    await this.createFeed(`${TEST_FEED_NAME} ${i}`, TEST_KEYWORDS);
   }
 });
 
